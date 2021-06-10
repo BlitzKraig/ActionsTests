085edc74c761852ca251c816db74c65244b6580d
BlitzKraig <craig@blitzkraig.com>
2021-06-10 21:50:27 +0100

    Update

--- .github/workflows/update-manifest.js
+++ .github/workflows/update-manifest.js
@@ -1,11 +1,7 @@
 var fs = require('fs');
-var newValues = fs.readFileSync('module-release.json', 'utf8')
-newValues.replace(/\$TAG/g, process.argv[2]||'latest')
-newValues = JSON.parse(newValues)
-var originalManifest = JSON.parse(fs.readFileSync('module.json', 'utf8'))
-
-
+var newValues = JSON.parse(fs.readFileSync('module-release.json', 'utf8'))
+var originalManifest = JSON.parse(fs.readFileSync('module.json', 'utf8').replace(/\$TAG/g, process.argv[2]||'latest'))
 const output = Object.assign({}, originalManifest, newValues);
 
 
-fs.writeFileSync('module.json', JSON.stringify(output));
+fs.writeFileSync('module.json', JSON.stringify(output, {}, 2));
