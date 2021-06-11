ad42a197c582329c6027cc9cc0d298bc49bd7ea4
BlitzKraig <craig@blitzkraig.com>
2021-06-11 20:54:14 +0100

    Updated

--- .github/workflows/update-manifest.js
+++ .github/workflows/update-manifest.js
@@ -1,27 +1,24 @@
 var fs = require('fs');
 var slugs = JSON.parse(fs.readFileSync('module-slugs.json', 'utf8'));
 var newValues = JSON.parse(fs.readFileSync('module-versions.json', 'utf8'));
-newValues.manifest = `${slugs.releaseUrl}${process.argv[2]||newValues.version}/${slugs.manifestName}`;
+newValues.manifest = `${slugs.releaseUrl}${process.argv[2]||newValues.version}/module.json`;
 newValues.download = `${slugs.releaseUrl}${process.argv[2]||newValues.version}/${slugs.downloadName}`;
 var originalManifest = JSON.parse(fs.readFileSync('module.json', 'utf8'));
 
 const output = Object.assign({}, originalManifest, newValues);
 
+
 fs.writeFileSync('module.json', JSON.stringify(output, {}, 2));
 
 var changelog = fs.readFileSync('latest-changes.md', 'utf8');
-// if(!process.argv[2]){
-// changelog += `
-
-// Archived Manifest Link: ${newValues.manifest}`
-// } else {
-//     changelog += `
-    
-// Manifest Link: ${newValues.manifest}`
-// }
+if(!process.argv[2]){
 changelog += `
 
-Manifest Link: ${newValues.manifest}`;
-
+Archived Manifest Link: ${newValues.manifest}`
+} else {
+    changelog += `
+    
+Manifest Link: ${newValues.manifest}`
+}
 
 fs.writeFileSync('latest-changes.md', changelog);
