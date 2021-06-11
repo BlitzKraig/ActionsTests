fe0e0c426abc26134d1c8cd8b0c0bc586b37d3af
BlitzKraig <craig@blitzkraig.com>
2021-06-11 21:13:09 +0100

    Updated

--- .github/workflows/update-manifest.js
+++ .github/workflows/update-manifest.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
 var slugs = JSON.parse(fs.readFileSync('module-slugs.json', 'utf8'));
 var newValues = JSON.parse(fs.readFileSync('module-versions.json', 'utf8'));
-newValues.manifest = `${slugs.releaseUrl}${process.argv[2]||newValues.version}/module.json`;
-newValues.download = `${slugs.releaseUrl}${process.argv[2]||newValues.version}/${slugs.downloadName}`;
+newValues.url = slugs.repoUrl;
+newValues.manifest = `${slugs.repoUrl}/releases/download/${process.argv[2]||newValues.version}/module.json`;
+newValues.download = `${slugs.repoUrl}/releases/download/${process.argv[2]||newValues.version}/${slugs.downloadName}`;
 var originalManifest = JSON.parse(fs.readFileSync('module.json', 'utf8'));
 
 const output = Object.assign({}, originalManifest, newValues);
@@ -14,11 +15,15 @@ var changelog = fs.readFileSync('latest-changes.md', 'utf8');
 if(!process.argv[2]){
 changelog += `
 
-Archived Manifest Link: ${newValues.manifest}`
+Archived Manifest Link: ${newValues.manifest}`;
+} else if(process.argv[2] == 'dev') {
+    changelog = `# Dev Release: Unstable!
+
+[Stable Release](${slugs.repoUrl}/releases/tag/latest)`;
 } else {
     changelog += `
     
-Manifest Link: ${newValues.manifest}`
+Manifest Link: ${newValues.manifest}`;
 }
 
 fs.writeFileSync('latest-changes.md', changelog);
